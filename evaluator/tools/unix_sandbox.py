a286699efe251d787fc978e03e6bd82b6bd809d3
KrisV123 <kristian.vesely@gmail.com>
2026-06-05 20:19:21 +0200

    fix: removed one limit for macOS, testing

--- evaluator/tools/unix_sandbox.py
+++ evaluator/tools/unix_sandbox.py
@@ -13,7 +13,7 @@ class UnixProcessAPI:
         setrlimit(resource.RLIMIT_CPU, (5, 5)) # type: ignore
         #setrlimit(resource.RLIMIT_AS, (200 * 1024 * 1024, -1)) # type: ignore
         setrlimit(resource.RLIMIT_NOFILE, (10, 10)) # type: ignore
-        #setrlimit(resource.RLIMIT_NPROC, (0, 0)) # type: ignore
+        setrlimit(resource.RLIMIT_NPROC, (0, 0)) # type: ignore
 
     @classmethod
     def create_unix_process(cls, cmd: list[str], input: str) -> str:
